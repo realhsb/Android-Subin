b689ba96177ae9226757236d5c326530c271d678
realhsb <realhsb@gmail.com>
2022-09-12 01:37:27 +0900

    Test6

---
+++ Doit-Android/Test6/app/src/main/java/com/example/test6/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.test6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.test6.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 바인딩 객체 획득
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        // 액티비티 화면 출력
+        setContentView(binding.root)
+
+        // 뷰 객체 이용
+        binding.visibleBtn.setOnClickListener {
+            binding.targetView.visibility = View.VISIBLE
+        }
+        binding.invisibleBtn.setOnClickListener {
+            binding.targetView.visibility = View.INVISIBLE
+        }
+    }
+}
