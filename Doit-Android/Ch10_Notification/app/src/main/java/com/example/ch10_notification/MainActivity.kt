bb5187e53f5034733957e7089e18ff859d35ddd6
realhsb <realhsb@gmail.com>
2022-09-12 01:37:48 +0900

    Ch10 Notification

---
+++ Doit-Android/Ch10_Notification/app/src/main/java/com/example/ch10_notification/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.ch10_notification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ch10_notification.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
